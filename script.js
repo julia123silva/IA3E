72a0bc22cbdbb9f601f3809494be103343e3cae8
julia123silva <161828004+julia123silva@users.noreply.github.com>
2024-08-02 09:28:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const pergunta = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"pergunta",
+        alternativa:[
+            "alternativa 1"
+            "alternativa 2"
+        ]
+    }
+
+]
