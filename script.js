42c8fc09f33ecac855a370375c2ab1b8ea4e2b99
julia123silva <161828004+julia123silva@users.noreply.github.com>
2024-10-03 07:49:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const pergunta = [//abre a lista de objetos (itens)
+const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"Você gosta da inteligencia artificial?"
-        alternativa:["Sim", "Não"]
+        alternativa:[{
+            texto:"Sim",
+            afirmação:"Porque ela é incrivel"
+            },
+            {
+                texto: "Não",
+                afirmação:""
+            }
+        ]
     },
     {
         enunciado: "Você acha a inteligencia artificial eficiente?"
-        alternativa: ["Sim", "Não"]
+        alternativa:[{
+            texto:"Sim",
+            afirmação:"Ate demais, ela cria e responde as melhores coisas que nem nós pensamos"
+            },
+            {
+                texto: "nao",
+                afirmação:"Porque a partir de um momento ela vai começar a controlar os afazeres dos seres humanos"
+            }
+        ]
     },
     {
         enunciado: "Você é de humanas ou exatas?"
-        alternativa: ["Exatas", "Humanas"]
+        alternativa: [{
+            texto:"Exatas",
+            afirmação:"me dou melhor com a matemática"
+            },
+            {
+                texto:"humanas",
+                afirmação:"sou melhor em linguagens"
+            }
+        ]
     }
 
 ]
@@ -29,7 +53,11 @@ function mostraPergunta(){
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativa){
         const botaoAlternativa = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventlistener("click", funcion(){
+            posição++;
+            mostraPergunta():
+        });
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 
